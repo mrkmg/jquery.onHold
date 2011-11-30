3d747430ddd86472d46248ce42af6bda75af5712
Kevin Gravier <kgravier@fusetg.com>
2011-11-30 13:59:01 -0500

    Added comments Added support for multiple objects Added default settings

--- jquery.onHold.js
+++ jquery.onHold.js
@@ -1,18 +1,39 @@
+/*
+This is free software, its code can be redistributed and/or modified at will for any purpose. Just leave this header here. Thanks
+--MrKmg
+*/
+
+/*
+
+Options:
+
+	time: 	Period of time a user should have to hold their mouse button down on an object to fire the event;
+			Measured in Milliseconds
+			Defaults to 5000
+*/
+
 (function( $ ){
-    $.fn.onHold= function(time,action) {
-        this.data('onHold_selected',false);
-        this.mousedown(function(){
-            $(this).data('onHold_selected',true);
-            var object = this;
-            setTimeout(function(){
-                if($(object).data('onHold_selected')){
-                     action();
-                }
-            },time);
-        });
-        this.mouseup(function(){
-            $(this).data('onHold_selected',false);
-        });
-       return this;
+    $.fn.onHold= function(callback,options) {
+		
+		var settings = $.extend({
+			time:5000
+		},options);
+		
+		return this.each(function(){
+			var $this = $(this);
+			$this.data('onHold_selected',false);
+			$this.mousedown(function(){
+				$this.data('onHold_selected',true);
+				setTimeout(function(){
+					if($this.data('onHold_selected')){
+						 callback();
+					}
+				},settings.time);
+			});
+			$this.mouseup(function(){
+				$this.data('onHold_selected',false);
+			});
+		});
+		
     };
 })( jQuery );
