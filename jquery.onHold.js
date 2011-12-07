2879857aa290b4b1be36fdf5e343524213e900a0
Kevin Gravier <kgravier@fusetg.com>
2011-12-07 14:53:25 -0500

    Updated Callback to now pass the event handler
    added option click to filter which mouse buttons should fire the event

--- jquery.onHold.js
+++ jquery.onHold.js
@@ -5,35 +5,65 @@ This is free software, its code can be redistributed and/or modified at will for
 
 /*
 
+callback : function(ev)
+
 Options:
 
 	time: 	Period of time a user should have to hold their mouse button down on an object to fire the event;
 			Measured in Milliseconds
 			Defaults to 5000
+	click: 	Which mouse buttons should count
+			left, middle, right, left_right, or all
+			defaults to both
+
 */
 
 (function( $ ){
     $.fn.onHold= function(callback,options) {
 		
 		var settings = $.extend({
-			time:5000
+			time:5000,
+			click:'all'
 		},options);
 		
 		return this.each(function(){
 			var $this = $(this);
 			$this.data('onHold_selected',false);
-			$this.mousedown(function(){
-				$this.data('onHold_selected',true);
-				setTimeout(function(){
-					if($this.data('onHold_selected')){
-						 callback();
-					}
-				},settings.time);
-			});
-			$this.mouseup(function(){
-				$this.data('onHold_selected',false);
+			$this.mousedown(function(ev){
+				var valid = false;
+				switch(settings.click)
+				{
+				case 'left':
+					if(ev.button == 0) valid = true;
+					break;
+				case 'right':
+					if(ev.button == 2) valid = true;
+					break;
+				case 'middle':
+					if(ev.button == 1) valid = true;
+					break;
+				case 'left_right':
+					if(ev.button == 0 || ev.button == 2) valid = true;
+					break;
+				case 'all':
+					valid = true;
+					break;
+				}
+				
+				if(valid)
+				{
+					$this.data('onHold_selected',true);
+					
+					setTimeout(function(){
+							if($this.data('onHold_selected')){
+								 callback(ev);
+							}},settings.time);
+
+					$this.mouseup(function(ev){
+						$this.data('onHold_selected',false);
+					});
+				}
 			});
 		});
-		
     };
 })( jQuery );
